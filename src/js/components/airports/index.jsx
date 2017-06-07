094e70f8d7b33adb973cf367795ec47141d5a0fa
Denis Bykov <dbykov@owm.io>
2017-06-07 10:34:29 +0300

    Работа с таблицей по отрисовке данных аэропорттов

--- src/js/components/airports/index.jsx
+++ src/js/components/airports/index.jsx
@@ -3,8 +3,7 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-import Airport from './airport';
+import DataTables from 'material-ui-datatables';
 
 export default class Airports extends Component {
   static get defaultProps() {
@@ -19,22 +18,72 @@ export default class Airports extends Component {
   }
   constructor(props) {
     super(props);
-    this.parseAirportsData = this.parseAirportsData.bind(this);
-  }
-  componentDidMount() {
-    this.parseAirportsData();
-  }
-  componentWillReceiveProps() {
-    this.parseAirportsData();
-  }
-  parseAirportsData() {
-    this.airports = this.props.infoData.map(elem => <Airport data={elem} key={elem.fs} />);
+    this.tableColumns = [
+      {
+        key: 'fs',
+        label: 'Fs'
+      }, {
+        key: 'name',
+        label: 'Name'
+      }, {
+        key: 'city',
+        label: 'City'
+      }, {
+        key: 'cityCode',
+        label: 'City Code'
+      }, {
+        key: 'countryName',
+        label: 'Country Name'
+      }, {
+        key: 'regionName',
+        label: 'Region Name'
+      }, {
+        key: 'localTime',
+        label: 'Local Time'
+      }, {
+        key: 'latitude',
+        label: 'Lat'
+      }, {
+        key: 'longitude',
+        label: 'Lon'
+      }, {
+        key: 'active',
+        label: 'Active'
+      }
+    ];
+    this.tableData = [
+      {
+        fs: this.props.infoData[0].fs || '',
+        name: this.props.infoData[0].name || '',
+        city: this.props.infoData[0].city || '',
+        cityCode: this.props.infoData[0].cityCode || '',
+        countryName: this.props.infoData[0].countryName || '',
+        regionName: this.props.infoData[0].regionName || '',
+        localTime: this.props.infoData[0].localTime || '',
+        latitude: this.props.infoData[0].latitude || '',
+        longitude: this.props.infoData[0].longitude || '',
+        active: this.props.infoData[0].active || 1,
+      }
+    ];
   }
   render() {
     return (
       <div>
         <h2> Airports </h2>
-        {this.airports}
+        <DataTables
+          height={'auto'}
+          selectable={false}
+          showRowHover={true}
+          columns={this.tableColumns}
+          data={this.tableData}
+          showCheckboxes={false}
+          onCellClick={this.handleCellClick}
+          onCellDoubleClick={this.handleCellDoubleClick}
+          onFilterValueChange={this.handleFilterValueChange}
+          onSortOrderChange={this.handleSortOrderChange}
+          page={1}
+          count={100}
+        />
       </div>
     );
   }
